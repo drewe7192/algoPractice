9cd141fcec20804849655b511b6e7e91840790b1
Felipe Ferreira <felipedferreira11@gmail.com>
2020-07-22 04:56:27 -0400

    adding to the SLL page

--- src/pages/SLLPage.tsx
+++ src/pages/SLLPage.tsx
@@ -1,5 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+
+import { PeopleCollection } from './../Data/PeopleSample'
+import { LinkedList } from './../datastructures/SLL'
+
 interface ISLLPageProps {}
 export const SLLPage: FC<ISLLPageProps> = (props: ISLLPageProps) => {
+
+    const [linkedList, changeLinkedList] = useState(LinkedList.createNewActions<IPerson>())
+
+    useEffect(() => {
+
+        const SLLInstance = LinkedList.createNewActions<IPerson>()
+
+        PeopleCollection.getFerreiraFamily().forEach(person => {
+            SLLInstance.push(person)
+        })
+
+    }, [])
+
     return <div>DISPLAY THAT YOU CAN USE SLL HERE!!!</div>
+
 }
